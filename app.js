d7bd63636df8110ba0b681b88ae088a14c06a40f
neymaru <106804514+neymaru@users.noreply.github.com>
2023-05-09 15:58:34 +0900

    express 리액트 빌드 배포

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const app = express();
 
 app.set("port", process.env.PORT || 3000);
 
+app.use(express.static(path.join(__dirname, "/build"))); // 경로에 /build 를 추가해 static path 로 지정
+
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/index.html")); // __dirname: 해당 파일이 존재하는 경로를 리턴
+  res.sendFile(path.join(__dirname, "/build/index.html")); // __dirname: 해당 파일이 존재하는 경로를 리턴
 });
 
 app.listen(app.get("port"), () => {
